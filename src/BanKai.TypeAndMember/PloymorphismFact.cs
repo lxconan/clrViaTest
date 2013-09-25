cf592868cc91ada5a63412a4a16ce2f117f82a25
lxconan <lxconan@gmail.com>
2013-09-26 02:39:17 +0800

    [liuxia] Add ploymorphism, readonly field, and ref type ctor.

--- src/BanKai.TypeAndMember/PloymorphismFact.cs
+++ src/BanKai.TypeAndMember/PloymorphismFact.cs
@@ -1,7 +1,72 @@
-﻿namespace BanKai.TypeAndMember
+﻿using Xunit;
+
+namespace BanKai.TypeAndMember
 {
     public class PloymorphismFact
     {
-         
+        private class Calculator
+        {
+            public int GetResult(int number)
+            {
+                return number * GetFactor();
+            }
+
+            protected virtual int GetFactor()
+            {
+                return 1;
+            }
+        }
+
+        private class CalculatorA : Calculator
+        {
+            protected override int GetFactor()
+            {
+                return 2;
+            }
+        }
+
+        // ReSharper disable once ClassWithVirtualMembersNeverInherited.Local
+        private class CalculatorB : Calculator
+        {
+            protected virtual new int GetFactor()
+            {
+                return 7;
+            }
+        }
+
+        // ReSharper disable once ClassWithVirtualMembersNeverInherited.Local
+        private class CalculatorC : Calculator
+        {
+            public new int GetResult(int number)
+            {
+                return base.GetResult(number) * 3;
+            }
+
+            protected virtual new int GetFactor()
+            {
+                return 8;
+            }
+        }
+
+        [Fact]
+        public void should_call_override_get_factor_method_for_polymorphism()
+        {
+            var calculatorA = new CalculatorA();
+            Assert.Equal(0, calculatorA.GetResult(9));
+        }
+
+        [Fact]
+        public void the_method_has_no_relation_ship_with_base_using_new_keyword()
+        {
+            var calculatorB = new CalculatorB();
+            Assert.Equal(0, calculatorB.GetResult(9));
+        }
+
+        [Fact]
+        public void a_more_complex_situation_using_new_keyword()
+        {
+            var calculatorC = new CalculatorC();
+            Assert.Equal(0, calculatorC.GetResult(9));
+        }
     }
 }
---
+++ src/BanKai.TypeAndMember/ReferenceTypeConstructorFact.cs
@@ -0,0 +1,75 @@
+﻿using Xunit;
+
+namespace BanKai.TypeAndMember
+{
+    public class ReferenceTypeConstructorFact
+    {
+        private class CtorSample
+        {
+            public int Value { get; private set; }
+
+            private CtorSample()
+            {
+                Value = 2;
+            }
+
+            public CtorSample(int value) : this()
+            {
+                Value *= value;
+            }
+
+            public CtorSample(int value1, int value2)
+            {
+                Value *= (value1 + value2);
+            }
+        }
+
+        [Fact]
+        public void calling_this_means_to_call_certain_ctor_of_current_type()
+        {
+            var ctorSample = new CtorSample(3);
+            Assert.Equal(0, ctorSample.Value);
+        }
+
+        [Fact]
+        public void calling_parameterless_ctor_of_current_type_is_not_a_default_behavior()
+        {
+            var ctorSample = new CtorSample(3, 4);
+            Assert.Equal(14, ctorSample.Value);
+        }
+
+        private class CtorSkipSample
+        {
+            public static int StaticValue { get; private set; }
+            public int InstanceValue { get; private set; }
+
+            static CtorSkipSample()
+            {
+                StaticValue = 1;
+            }
+
+            public CtorSkipSample()
+            {
+                ++StaticValue;
+                InstanceValue = 5;
+            }
+
+            public CtorSkipSample MemberWiseCloneTest()
+            {
+                return (CtorSkipSample)MemberwiseClone();
+            }
+        }
+
+        [Fact]
+        public void using_member_wise_clone_will_not_call_ctor()
+        {
+            var ctorSkipSample = new CtorSkipSample();
+            Assert.Equal(0, CtorSkipSample.StaticValue);
+            Assert.Equal(0, ctorSkipSample.InstanceValue);
+
+            var anotherCtorSkipSample = ctorSkipSample.MemberWiseCloneTest();
+            Assert.Equal(0, CtorSkipSample.StaticValue);
+            Assert.Equal(0, anotherCtorSkipSample.InstanceValue);
+        }
+    }
+}
