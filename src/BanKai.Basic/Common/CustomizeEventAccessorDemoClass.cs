e03ba34a0e2a7b91ca4aa4c3b45b3bda8db20a01
Liu Xia <lxconan@gmail.com>
2014-12-11 23:33:13 +0800

    Complete event related facts. Increase order number by 1 because we have to do anonymous method first.

---
+++ src/BanKai.Basic/Common/CustomizeEventAccessorDemoClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BanKai.Basic.Common
+{
+    internal class CustomizeEventAccessorDemoClass
+    {
+        private EventHandler m_event;
+
+        public event EventHandler Event
+        {
+            add
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("value");
+                }
+
+                m_event += value;
+            }
+            
+            // ReSharper disable once DelegateSubtraction
+            remove { m_event -= value; }
+        }
+
+        private void OnTiggered()
+        {
+            var handler = m_event;
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+
+        public void Tigger()
+        {
+            OnTiggered();
+        }
+    }
+}
---
+++ src/BanKai.Basic/Common/CustomizeEventArgsDemoClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BanKai.Basic.Common
+{
+    internal class CustomizeEventArgsDemoClass
+    {
+        public event EventHandler<GreetingEventArgs> Greeting;
+
+        public void Greet(string name)
+        {
+            OnGreeting("Hello " + name);
+        }
+
+        private void OnGreeting(string content)
+        {
+            EventHandler<GreetingEventArgs> handler = Greeting;
+            if (handler != null)
+            {
+                handler(this, new GreetingEventArgs(content));
+            }
+        }
+    }
+
+    internal class GreetingEventArgs : EventArgs
+    {
+        private readonly string m_greetingContent;
+
+        public GreetingEventArgs(string greetingContent)
+        {
+            m_greetingContent = greetingContent;
+        }
+
+        public string GreetingContent
+        {
+            get { return m_greetingContent; }
+        }
+    }
+}
