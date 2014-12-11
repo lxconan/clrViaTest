74c0acebc3f324eb7db1f23dea359d39195f7276
Liu Xia <lxconan@gmail.com>
2014-12-12 01:17:36 +0800

    [liuxia] Add disposable related facts.

---
+++ src/BanKai.Basic/Common/DisposableWithTracingDemoClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace BanKai.Basic.Common
+{
+    public class DisposableWithTracingDemoClass : IDisposable
+    {
+        private readonly StringBuilder m_tracer;
+
+        public DisposableWithTracingDemoClass(StringBuilder tracer)
+        {
+            m_tracer = tracer;
+            m_tracer.AppendLine("constructor called.");
+        }
+
+        public void Dispose()
+        {
+            m_tracer.AppendLine("dispose called.");
+        }
+    }
+}
---
+++ src/BanKai.Basic/Common/TryCatchDemoClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BanKai.Basic.Common
+{
+    internal class TryCatchDemoClass
+    {
+        public void WillNotThrowException()
+        {
+            // This method throws nothing :-D
+        }
+
+        public void WillThrowFormatException()
+        {
+            throw new FormatException();
+        }
+    }
+}
