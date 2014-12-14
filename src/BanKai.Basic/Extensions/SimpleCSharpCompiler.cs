aea6e2ee1ee8ff5b884b497cb4ced72b2ab9b8a7
Liu Xia <lxconan@gmail.com>
2014-12-14 22:19:45 +0800

    [liuxia] Add nullable type facts.

---
+++ src/BanKai.Basic/Extensions/SimpleCSharpCompiler.cs
@@ -0,0 +1,21 @@
+﻿using System.CodeDom.Compiler;
+
+namespace BanKai.Basic.Extensions
+{
+    internal static class SimpleCSharpCompiler
+    {
+        public static CompilerResults CompileWithoutRun(string shortProgram)
+        {
+            CodeDomProvider csharpCodeProvider = CodeDomProvider.CreateProvider("CSharp");
+            var compilerParameters = new CompilerParameters
+            {
+                GenerateInMemory = true,
+                TreatWarningsAsErrors = false
+            };
+
+            CompilerResults compilerResults =
+                csharpCodeProvider.CompileAssemblyFromSource(compilerParameters, shortProgram);
+            return compilerResults;
+        }
+    }
+}
