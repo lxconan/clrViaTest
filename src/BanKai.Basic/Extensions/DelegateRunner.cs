546a4f9392695a7b6e4deace4b327a48ea76be01
Liu Xia <lxconan@gmail.com>
2014-12-14 21:18:00 +0800

    [liuxia] Add enumeration facts.

---
+++ src/BanKai.Basic/Extensions/DelegateRunner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BanKai.Basic.Extensions
+{
+    internal static class DelegateRunner
+    {
+        public static Exception RunAndGetUnhandledException(this Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception error)
+            {
+                return error;
+            }
+
+            return null;
+        }
+    }
+}
