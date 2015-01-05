93cd533da7c49c5ae8bcb81a7babf9cf619b4b94
Liu Xia <lxconan@gmail.com>
2015-01-05 22:48:38 +0800

    [liuxia] Add scene based practice project.

---
+++ src/BanKai.Advanced.Scene/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BanKai.Advanced.Scene")]
+[assembly: AssemblyDescription("Testing practice on scene based questions.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Liu Xia")]
+[assembly: AssemblyProduct("BanKai.Advanced.Scene")]
+[assembly: AssemblyCopyright("Copyright © Liu Xia 2014-2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("23e517f3-cf7f-485e-b514-e91f21a3f8dc")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
